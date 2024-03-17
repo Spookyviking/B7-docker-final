5cd6a80119c55734cc51209d82c8a7cd4c516ba4
Maksim Ostapenko <spookyvikingooth@gmail.com>
2024-03-17 23:59:56 +0300

    test fix

--- web.py
+++ web.py
@@ -12,27 +12,19 @@ def get_db_config(db_option):
         config.read('/srv/app/conf/web.conf')
         try:
                 result = config.get("database", db_option)
-#        except configparser.NoSectionError:
-        except NoSectionError:
-#                print ('Cannot get {}. There is no such section or config file is unavailable/does not exist').format(db_option)
-#                exit ()
-                result = 'Configuration Error: Could not retrieve {}'.format(db_option)
+        except configparser.NoSectionError:
+                print ('Cannot get {}. There is no such section or config file is unavailable/does not exist').format(db_option)
+                exit ()
         return result
 
 
 def get_db_time():
-    conn = connect(
-        host=get_db_config('db_host'), 
-        port=get_db_config('db_port'), 
-        dbname=get_db_config('db_name'), 
-        user=get_db_config('db_user'), 
-        password=get_db_config('db_password')
-    )
-    conn.autocommit = True
-    cur = conn.cursor()
-    cur.execute('SELECT current_user;')
-    return cur.fetchone()
-        
+        conn = connect("host= {0} dbname={1} user={2} password={3}".format(get_db_config('db_host'), get_db_config('db_name'), get_db_config('db_user'), get_db_config('db_password')))
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute('SELECT current_user;;')
+        return cur.fetchone()
+
 
 text = """<h1 style='color:blue'>Hello there!</h1>
 Everything is OK! DB Query was completed by {} user""".format(get_db_time()[0])
